be1ca3abdd28803f9a622651b78a2f939ff7eff6
damjanh <damjan.hudobreznik@gmail.com>
2022-06-22 12:10:17 +0200

    Main component stubs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import WorkoutList from "./components/WorkoutList";
+import CreateWorkout from "./components/CreateWorkout";
+import CreateUser from "./components/CreateUser";
+import EditWorkout from "./components/EditWorkout";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <br/>
       <Routes>
         <Route path="/" element={<WorkoutList/>} />
+        <Route path="/edit/:id" element={<EditWorkout/>} />
+        <Route path="/create" element={<CreateWorkout/>} />
+        <Route path="/user" element={<CreateUser/>} />
       </Routes>
     </Router>
   );
---
+++ src/components/CreateWorkout.js
@@ -0,0 +1,9 @@
+const CreateWorkout = () => {
+  return (
+    <div>
+      <h1>Create Workout</h1>
+    </div>
+  )
+}
+
+export default CreateWorkout
