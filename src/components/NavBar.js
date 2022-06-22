8a74902c85c0068c752a79ea6d2117debf1983a5
damjanh <damjan.hudobreznik@gmail.com>
2022-06-22 12:05:05 +0200

    Implement basic navigtaion bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,23 @@
+import { Link } from 'react-router-dom'
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+      <Link to='/' className='navbar-brand'> Workout Tracker</Link>
+      <div className='collpase navbar-collapse'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <Link to='/' className='nav-link'>Home</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/create' className='nav-link'>Create Workout Log</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/user' className='nav-link'>Create User</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
