c82fea911733af3ad2ff80746e836a4abad2fe77
damjanh <damjan.hudobreznik@gmail.com>
2022-06-22 11:55:00 +0200

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import WorkoutList from "./components/WorkoutList";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Works</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <br/>
+      <Routes>
+        <Route path="/" element={<WorkoutList/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/WorkoutList.js
@@ -0,0 +1,9 @@
+const WorkoutList = () => {
+  return (
+    <div>
+      <h1>Workout List</h1>
+    </div>
+  )
+}
+
+export default WorkoutList
